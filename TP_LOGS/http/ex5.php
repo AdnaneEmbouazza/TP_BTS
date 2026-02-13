30a12f7c90ba6a0fe265165821d0d6710675f02c
adnane EMBOUAZZA <aembouazza@turgot-paris.si>
2026-02-13 11:11:46 +0100

    ajout ex5

---
+++ TP_LOGS/http/ex5.php
@@ -0,0 +1,17 @@
+<?php
+
+$fic = fopen("access.log", "r");
+$i= 0 // compteur de ligne 
+
+while (!feof($fic)){
+    $ligne=fgets($fic, 1024);
+    if(preg_match('/" 404/' , $ligne)){
+        $i++.
+        preg_match('/GET \/[a-zA-ZO-9._-]*/' , $ligne , $matches);
+        var_dump($matches);
+    }
+}
+var_dump($i);
+fclose($fic);
+
+?>
