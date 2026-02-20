f77a9049c90a640283be1c43150ddacf680920dd
Adnane EMBOUAZZA <adnaneembouazza77000@gmail.com>
2026-02-20 12:24:23 +0100

    modif express session

--- tpexpresssessionActivite1/index.js
+++ tpexpresssessionActivite1/index.js
@@ -11,16 +11,21 @@ app.use(session({
     saveUninitialized: true //deprecated si absent
 }))
 
-app.use(function (req, res, next) {
-    if (!req.session.views) {
-        req.session.views = {}
-    }
-    // get the url pathname
-    var pathname = parseurl(req).pathname
-    // count the views
-    req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-    next()
-})
+pp.use(function (req, res, next) {
+ if (!req.session.views) {
+ req.session.views = {}
+ }
+ // get the url pathname
+ var pathname = parseurl(req).pathname
+ // count the views - utilise la clé pathname pour /foo et /bar, sinon '404'
+ if (pathname === '/foo' || pathname === '/bar') {
+ req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+ } else {
+ req.session.views['404'] = (req.session.views['404'] || 0) + 1
+ }
+ next()
+}) 
+
 
 app.get('/foo', function (req, res, next) {
     res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
@@ -31,6 +36,15 @@ app.get('/bar', function (req, res, next) {
     res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
     console.log(req.session.views)
 })
+
+app.use(function (req, res) {
+ var count = req.session.views['404'] || 0
+ res.status(404).send(`
+ <h1>404 - Page ${req.path} non trouvée</h1>
+ `)
+ console.log(req.session.views)
+}) 
+
 
 app.listen(3000)
 console.log('Listening on http://localhost:3000')
